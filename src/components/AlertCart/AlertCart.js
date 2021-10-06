847be69432c60162bd7185bab43d1ea81f0882d6
Damián <damiancabrio@hotmail.com>
2021-10-05 23:50:24 -0300

    Refactorizacion de AlertCart

--- src/components/AlertCart/AlertCart.js
+++ src/components/AlertCart/AlertCart.js
@@ -1,18 +1,19 @@
 import { Alert } from "react-bootstrap";
 
+const message = {
+  success: {
+    title: "Su compra se ha realizado con exito",
+    message:
+      "Pronto nos estaremos comunicando con usted. Mientras tanto puede seguir mirando nuestro catalogo.",
+  },
+  danger: {
+    title: "Ocurrio un error",
+    message:
+      "Por favor vuelva a intentar, si el error persiste pongase en contacto con el administrador del sitio",
+  },
+};
+
 const AlertCart = ({ variation, setShow }) => {
-  const message = {
-    success: {
-      title: "Su compra se ha realizado con exito",
-      message:
-        "Pronto nos estaremos comunicando con usted. Mientras tanto puede seguir mirando nuestro catalogo.",
-    },
-    danger: {
-      title: "Ocurrio un error",
-      message:
-        "Por favor vuelva a intentar, si el error persiste pongase en contacto con el administrador del sitio",
-    },
-  };
 
   return (
     <Alert variant={variation} onClose={() => setShow(false)} dismissible>
