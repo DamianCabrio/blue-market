56449808b2f7b5b03a0b55a9e15b3b481947ba83
Damián <damiancabrio@hotmail.com>
2021-10-05 23:31:03 -0300

    Arreglo de estilos de CartWidget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,18 +1,13 @@
 import { Badge } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { useCartContext } from './../../context/cartContext';
+import { useCartContext } from "./../../context/cartContext";
 
 function CartWidget() {
   const { cartTotals } = useCartContext();
 
   return (
-    <Link to="/cart">
-      <img
-        src="/img/shopping-cart.svg"
-        alt="Carrito"
-        width="25px"
-        className="carrito"
-      />
+    <Link to="/cart" className="d-grid" id="cartContainer">
+      <img src="/img/shopping-cart.svg" alt="Carrito" width="25px" />
       <Badge bg="primary">{cartTotals.amount}</Badge>
     </Link>
   );
